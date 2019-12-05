dbeb230623e7f860b0cbdbf4aca962a4b0f72216
Tyler Hancock <darklime@live.ca>
2019-12-05 04:02:23 -0700

    Added models and dyed pots and stuff.

--- src/main/java/net/darkhax/botanypots/BotanyPots.java
+++ src/main/java/net/darkhax/botanypots/BotanyPots.java
@@ -1,5 +1,8 @@
 package net.darkhax.botanypots;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,6 +12,7 @@ import net.darkhax.botanypots.api.crop.CropReloadListener;
 import net.darkhax.botanypots.api.fertilizer.FertilizerReloadListener;
 import net.darkhax.botanypots.api.soil.SoilReloadListener;
 import net.minecraft.block.Block;
+import net.minecraft.item.DyeColor;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.resources.IReloadableResourceManager;
 import net.minecraft.tileentity.TileEntityType;
@@ -27,19 +31,26 @@ public class BotanyPots {
     private final RegistryHelper registry;
     
     public static TileEntityType<TileEntityBotanyPot> tileBotanyPot;
-    public static Block blockBotanyPot;
+    public static List<Block> botanyPots = new ArrayList<>();
     
     public BotanyPots() {
         
         this.registry = DistExecutor.runForDist( () -> () -> new RegistryHelperClient(MOD_ID, LOGGER, ITEM_GROUP), () -> () -> new RegistryHelper(MOD_ID, LOGGER, ITEM_GROUP));
         MinecraftForge.EVENT_BUS.addListener(this::startServer);
-        blockBotanyPot = this.registry.registerBlock(new BlockBotanyPot(), "botany_pot");
-        tileBotanyPot = this.registry.registerTileEntity(TileEntityBotanyPot::new, "botany_pot", blockBotanyPot);
+        botanyPots.add(this.registry.registerBlock(new BlockBotanyPot(), "botany_pot"));
+        
+        for (DyeColor dyeColor : DyeColor.values()) {
+            
+            botanyPots.add(this.registry.registerBlock(new BlockBotanyPot(), dyeColor.getName() + "_botany_pot"));
+        }
+        
+        tileBotanyPot = this.registry.registerTileEntity(TileEntityBotanyPot::new, "botany_pot", botanyPots.toArray(new Block[0]));
         
         if (this.registry instanceof RegistryHelperClient) {
             
             ((RegistryHelperClient) this.registry).setSpecialRenderer(TileEntityBotanyPot.class, new TileEntityRendererBotanyPot());
         }
+        
         this.registry.initialize(FMLJavaModLoadingContext.get().getModEventBus());
     }
     
