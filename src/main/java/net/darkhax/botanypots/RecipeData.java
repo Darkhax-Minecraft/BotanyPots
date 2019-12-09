f6d81122daffa6b0a0a2f546c50615d1c8986f46
Tyler Hancock <darklime@live.ca>
2019-12-08 23:49:28 -0700

    Added a check for stupid issues.

--- src/main/java/net/darkhax/botanypots/RecipeData.java
+++ src/main/java/net/darkhax/botanypots/RecipeData.java
@@ -7,6 +7,19 @@ import net.minecraft.world.World;
 
 public abstract class RecipeData implements IRecipe<IInventory> {
 
+    public RecipeData() {
+        
+        if (this.getSerializer() == null) {
+            
+            throw new IllegalStateException("No serializer found for " + this.getClass().getName());
+        }
+        
+        if (this.getType() == null) {
+            
+            throw new IllegalStateException("No recipe type found for " + this.getClass().getName());
+        }
+    }
+    
     @Override
     public boolean matches (IInventory inv, World worldIn) {
         
