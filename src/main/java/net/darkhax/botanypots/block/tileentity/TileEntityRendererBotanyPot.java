919249bc465831fed738c2e21e53d9b2447e2920
Tyler Hancock <darklime@live.ca>
2020-03-09 15:04:18 -0600

    Fixed rendering to support biome colours and render layers.

--- src/main/java/net/darkhax/botanypots/block/tileentity/TileEntityRendererBotanyPot.java
+++ src/main/java/net/darkhax/botanypots/block/tileentity/TileEntityRendererBotanyPot.java
@@ -1,14 +1,27 @@
 package net.darkhax.botanypots.block.tileentity;
 
+import java.util.Random;
+
 import com.mojang.blaze3d.matrix.MatrixStack;
 
+import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.ForgeHooksClient;
+import net.minecraftforge.client.model.ModelDataManager;
+import net.minecraftforge.client.model.data.IModelData;
 
 @OnlyIn(Dist.CLIENT)
 public class TileEntityRendererBotanyPot extends TileEntityRenderer<TileEntityBotanyPot> {
@@ -20,15 +33,13 @@ public class TileEntityRendererBotanyPot extends TileEntityRenderer<TileEntityBo
     
     @Override
     public void render (TileEntityBotanyPot tile, float partial, MatrixStack matrix, IRenderTypeBuffer buffer, int light, int overlay) {
-        
-        final Minecraft mc = Minecraft.getInstance();
-        
+
         if (tile.getSoil() != null) {
             
             matrix.push();
             matrix.scale(0.625f, 0.384f, 0.625f);
             matrix.translate(0.3, 0.01, 0.3);
-            mc.getBlockRendererDispatcher().renderBlock(tile.getSoil().getRenderState(), matrix, buffer, light, overlay);
+            renderBlock(tile.getSoil().getRenderState(), tile.getWorld(), tile.getPos(), matrix, buffer);
             matrix.pop();
             
             if (tile.getCrop() != null) {
@@ -38,9 +49,27 @@ public class TileEntityRendererBotanyPot extends TileEntityRenderer<TileEntityBo
                 matrix.translate(0.5, 0.40, 0.5);
                 matrix.scale(growth, growth, growth);
                 matrix.translate(-0.5, 0, -0.5);
-                mc.getBlockRendererDispatcher().renderBlock(tile.getCrop().getDisplayState(), matrix, buffer, light, overlay);
+                renderBlock(tile.getCrop().getDisplayState(), tile.getWorld(), tile.getPos(), matrix, buffer);
                 matrix.pop();
             }
         }
+    }
+    
+    private void renderBlock(BlockState state, World world, BlockPos pos, MatrixStack matrix, IRenderTypeBuffer buffer) {
+
+        final BlockRendererDispatcher dispatcher = Minecraft.getInstance().getBlockRendererDispatcher();
+        final IBakedModel model = dispatcher.getModelForState(state);
+        final IModelData data = model.getModelData(world, pos, state, ModelDataManager.getModelData(world, pos));
+        
+        for (RenderType type : RenderType.getBlockRenderTypes()) {
+            
+           if (RenderTypeLookup.canRenderInLayer(state, type)) {
+               
+              ForgeHooksClient.setRenderLayer(type);
+              dispatcher.getBlockModelRenderer().renderModel(world, model, state, pos, matrix, buffer.getBuffer(type), false, new Random(), state.getPositionRandom(pos), OverlayTexture.NO_OVERLAY, data);
+           }
+        }
+        
+        ForgeHooksClient.setRenderLayer(null);
     }
 }
