1b07576a45d527f68ebcb56d69f74cc2c4e6f82f
Tyler Hancock <darklime@live.ca>
2020-03-08 22:03:26 -0600

    Removed seed tooltip in favor of JEI support.

--- src/main/java/net/darkhax/botanypots/BotanyPots.java
+++ src/main/java/net/darkhax/botanypots/BotanyPots.java
@@ -5,14 +5,8 @@ import org.apache.logging.log4j.Logger;
 
 import net.darkhax.bookshelf.item.ItemGroupBase;
 import net.darkhax.bookshelf.registry.RegistryHelper;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -36,19 +30,7 @@ public class BotanyPots {
         this.registry = new RegistryHelper(MOD_ID, LOGGER, this.itemGroup);
         this.content = DistExecutor.runForDist( () -> () -> new ContentClient(this.registry), () -> () -> new Content(this.registry));
         
-        DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> MinecraftForge.EVENT_BUS.addListener(this::seedTooltip));
-        
         this.registry.initialize(FMLJavaModLoadingContext.get().getModEventBus());
-    }
-    
-    private void seedTooltip (ItemTooltipEvent event) {
-        
-        final PlayerEntity player = event.getPlayer();
-        
-        if (event.getFlags().isAdvanced() && player != null && BotanyPotHelper.getCropForItem(player.world, event.getItemStack()) != null) {
-            
-            event.getToolTip().add(new TranslationTextComponent("botanypots.tooltip.advanced.seed_item").applyTextStyle(TextFormatting.GREEN));
-        }
     }
     
     public Content getContent () {
