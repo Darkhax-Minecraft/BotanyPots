81b5e0daad6ef5d1e449a92459c239fde0be4451
Tyler Hancock <darklime@live.ca>
2020-09-18 09:01:35 -0600

    Re-added Hwyla support. #67

---
+++ src/main/java/net/darkhax/botanypots/addons/hwyla/BotanyPotsHwylaPlugin.java
@@ -0,0 +1,31 @@
+package net.darkhax.botanypots.addons.hwyla;
+
+import mcp.mobius.waila.api.IRegistrar;
+import mcp.mobius.waila.api.IWailaPlugin;
+import mcp.mobius.waila.api.TooltipPosition;
+import mcp.mobius.waila.api.WailaPlugin;
+import net.darkhax.botanypots.BotanyPots;
+import net.darkhax.botanypots.block.BlockBotanyPot;
+import net.minecraft.util.ResourceLocation;
+
+@WailaPlugin
+public class BotanyPotsHwylaPlugin implements IWailaPlugin {
+    
+    public static final ResourceLocation KEY_SHOW_SOIL = new ResourceLocation(BotanyPots.MOD_ID, "show_soil");
+    public static final ResourceLocation KEY_SHOW_CROP = new ResourceLocation(BotanyPots.MOD_ID, "show_crop");
+    public static final ResourceLocation KEY_SHOW_PROGRESS = new ResourceLocation(BotanyPots.MOD_ID, "show_progress");
+    public static final ResourceLocation KEY_SHOW_TIME = new ResourceLocation(BotanyPots.MOD_ID, "show_time");
+    public static final ResourceLocation KEY_SHOW_DEBUG = new ResourceLocation(BotanyPots.MOD_ID, "show_debug");
+    
+    @Override
+    public void register (IRegistrar registry) {
+        
+        registry.addConfig(KEY_SHOW_SOIL, true);
+        registry.addConfig(KEY_SHOW_CROP, true);
+        registry.addConfig(KEY_SHOW_PROGRESS, true);
+        registry.addConfig(KEY_SHOW_DEBUG, false);
+        registry.addConfig(KEY_SHOW_TIME, true);
+        
+        registry.registerComponentProvider(new BotanyPotComponentProvider(), TooltipPosition.BODY, BlockBotanyPot.class);
+    }
+}
