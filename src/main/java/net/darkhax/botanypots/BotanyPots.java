914edb66a4d950eeacefd24f09970cb5bbfcefe4
Tyler Hancock <darklime@live.ca>
2020-09-18 09:58:35 -0600

    Added TheOneProbe support. #67

--- src/main/java/net/darkhax/botanypots/BotanyPots.java
+++ src/main/java/net/darkhax/botanypots/BotanyPots.java
@@ -6,13 +6,17 @@ import org.apache.logging.log4j.Logger;
 import net.darkhax.bookshelf.item.ItemGroupBase;
 import net.darkhax.bookshelf.network.NetworkHelper;
 import net.darkhax.bookshelf.registry.RegistryHelper;
+import net.darkhax.bookshelf.util.ModUtils;
+import net.darkhax.botanypots.addons.top.TOPPlugin;
 import net.darkhax.botanypots.network.BreakEffectsMessage;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig.Type;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BotanyPots.MOD_ID)
@@ -40,6 +44,15 @@ public class BotanyPots {
         this.content = DistExecutor.unsafeRunForDist( () -> () -> new ContentClient(this.registry), () -> () -> new Content(this.registry));
         
         this.registry.initialize(FMLJavaModLoadingContext.get().getModEventBus());
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+    }
+    
+    private void setup (FMLCommonSetupEvent event) {
+        
+        if (ModUtils.isInModList("theoneprobe")) {
+            
+            InterModComms.sendTo("theoneprobe", "getTheOneProbe", TOPPlugin::new);
+        }
     }
     
     public Content getContent () {
