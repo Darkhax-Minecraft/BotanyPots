d84b657e3e58e59c3dadf8cadc3d992186c2f4d2
Tyler Hancock <darklime@live.ca>
2021-01-15 20:23:41 -0700

    Cleaned up /ct dump support and allowed the message to be translated.

--- src/main/java/net/darkhax/botanypots/addons/crt/BotanyDump.java
+++ src/main/java/net/darkhax/botanypots/addons/crt/BotanyDump.java
@@ -1,7 +1,7 @@
 package net.darkhax.botanypots.addons.crt;
 
 import java.util.Map;
-import java.util.Set;
+import java.util.function.Function;
 
 import com.blamejared.crafttweaker.CraftTweaker;
 import com.blamejared.crafttweaker.api.CraftTweakerAPI;
@@ -13,63 +13,37 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.RecipeManager;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
+import net.minecraft.util.text.TranslationTextComponent;
 
 public class BotanyDump implements CTCommands.CommandCallerPlayer {
+    
     private final String dumpCommandName;
     private final String dumpedContentName;
-    private final DumpConverter dumpConverter;
+    private final Function<RecipeManager, Map<ResourceLocation, ?>> dumpConverter;
+    private final ITextComponent positiveFeedback;
     
-    public BotanyDump(String dumpCommandName, String dumpedContentName, DumpConverter dumpConverter) {
+    public BotanyDump(String dumpCommandName, String dumpedContentName, Function<RecipeManager, Map<ResourceLocation, ?>> dumpConverter) {
         
         this.dumpCommandName = dumpCommandName;
         this.dumpedContentName = dumpedContentName;
         this.dumpConverter = dumpConverter;
+        this.positiveFeedback = new TranslationTextComponent("botanypots.log.crt.dump", this.dumpCommandName).mergeStyle(TextFormatting.GREEN);
     }
     
     @Override
-    public int executeCommand (PlayerEntity playerEntity, ItemStack itemStack) {
+    public int executeCommand (PlayerEntity player, ItemStack stack) {
         
-        this.dumpToLog(playerEntity);
-        this.sendFeedBack(playerEntity);
+        final RecipeManager recipeManager = player.getEntityWorld().getRecipeManager();      
+        CraftTweakerAPI.logDump("List of all known %s:", this.dumpedContentName);       
+        this.dumpConverter.apply(recipeManager).keySet().forEach(id -> CraftTweakerAPI.logDump("- %s", id));       
+        player.sendMessage(this.positiveFeedback, CraftTweaker.CRAFTTWEAKER_UUID);
         return 0;
-    }
-    
-    private void sendFeedBack (PlayerEntity player) {
-        
-        final String format = "%sList of %s info generated! Check the crafttweaker.log file!%s";
-        final String message = String.format(format, TextFormatting.GREEN, this.dumpedContentName, TextFormatting.RESET);
-        final ITextComponent toSend = new StringTextComponent(message);
-        
-        player.sendMessage(toSend, CraftTweaker.CRAFTTWEAKER_UUID);
-    }
-    
-    private void dumpToLog (PlayerEntity player) {
-        
-        final RecipeManager recipeManager = player.getEntityWorld().getRecipeManager();
-        final Set<ResourceLocation> locationsToDump = this.dumpConverter.getFromManager(recipeManager).keySet();
-        
-        CraftTweakerAPI.logDump("List of all known %s:", this.dumpedContentName);
-        
-        for (final ResourceLocation location : locationsToDump) {
-            CraftTweakerAPI.logDump("- %s", location);
-        }
     }
     
     public void registerTo (CTCommandCollectionEvent event) {
         
-        final String description = this.getDescription();
-        event.registerDump(this.dumpCommandName, description, this);
-        
-    }
-    
-    private String getDescription () {
-        
-        return String.format("Outputs all known %s names to the log.", this.dumpedContentName);
-    }
-    
-    interface DumpConverter {
-        Map<ResourceLocation, ?> getFromManager (RecipeManager manager);
+        final String description = String.format("Outputs all known %s names to the log.", this.dumpedContentName);
+        event.registerDump(this.dumpCommandName, description, this);        
     }
 }
