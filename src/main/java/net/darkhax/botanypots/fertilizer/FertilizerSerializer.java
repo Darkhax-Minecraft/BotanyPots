e4ff55ba7e76c18fa41f66cb819cc67eeba99d95
Tyler Hancock <darklime@live.ca>
2021-02-05 09:20:29 -0700

    Fixed potential for fertilizers to int underflow/overflow. Closes #137

--- src/main/java/net/darkhax/botanypots/fertilizer/FertilizerSerializer.java
+++ src/main/java/net/darkhax/botanypots/fertilizer/FertilizerSerializer.java
@@ -1,11 +1,16 @@
 package net.darkhax.botanypots.fertilizer;
 
+import java.math.BigInteger;
+
+import javax.annotation.Nullable;
+
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.JsonSyntaxException;
 
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.network.PacketBuffer;
-import net.minecraft.util.JSONUtils;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.registries.ForgeRegistryEntry;
 
@@ -17,8 +22,13 @@ public class FertilizerSerializer extends ForgeRegistryEntry<IRecipeSerializer<?
     public FertilizerInfo read (ResourceLocation id, JsonObject json) {
         
         final Ingredient ingredient = Ingredient.deserialize(json.getAsJsonObject("fertilizer"));
-        final int minTicks = JSONUtils.getInt(json, "minTicks");
-        final int maxTicks = JSONUtils.getInt(json, "maxTicks");
+        final int minTicks = readIntClamped(json, "minTicks", 0, Integer.MAX_VALUE, null);
+        final int maxTicks = readIntClamped(json, "maxTicks", minTicks, Integer.MAX_VALUE, null);
+        
+        if (minTicks > maxTicks || maxTicks < minTicks) {
+            
+            throw new JsonSyntaxException("minTicks (" + minTicks + ") must be less than or equal to maxTicks (" + maxTicks + ").");
+        }
         
         return new FertilizerInfo(id, ingredient, minTicks, maxTicks);
     }
@@ -39,5 +49,42 @@ public class FertilizerSerializer extends ForgeRegistryEntry<IRecipeSerializer<?
         info.getIngredient().write(buffer);
         buffer.writeInt(info.getMinTicks());
         buffer.writeInt(info.getMaxTicks());
+    }
+    
+    private static int readIntClamped (JsonObject json, String memberName, int min, int max, @Nullable Integer defaultValue) {
+        
+        if (json.has(memberName)) {
+            
+            final BigInteger bigMin = BigInteger.valueOf(min);
+            final BigInteger bigMax = BigInteger.valueOf(max);
+            final BigInteger bigInt = new BigInteger(json.get(memberName).getAsString());
+            
+            if (bigInt.compareTo(bigMin) < 0) {
+                
+                throw new JsonParseException("Property " + memberName + " is too small. Must be greater than or equal to " + min);
+            }
+            
+            if (bigInt.compareTo(bigMax) > 0) {
+                
+                throw new JsonParseException("Property " + memberName + " is too big. Must be less than or equal to " + max);
+            }
+            
+            try {
+                
+                return bigInt.intValueExact();
+            }
+            
+            catch (final ArithmeticException e) {
+                
+                throw new JsonParseException("Expected property " + memberName + " to be an integer but it's value is out of range.", e);
+            }
+        }
+        
+        if (defaultValue != null) {
+            
+            return defaultValue;
+        }
+        
+        throw new JsonParseException("Property " + memberName + " was undefined.");
     }
 }
