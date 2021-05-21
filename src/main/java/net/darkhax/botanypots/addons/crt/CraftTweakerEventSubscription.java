f9543e5a23b052b1837a2d31b8919816858d3668
Tyler Hancock <darklime@live.ca>
2021-05-21 16:14:10 -0600

    Remove deprecated CraftTweaker examples.

--- src/main/java/net/darkhax/botanypots/addons/crt/CraftTweakerEventSubscription.java
+++ src/main/java/net/darkhax/botanypots/addons/crt/CraftTweakerEventSubscription.java
@@ -1,23 +1,11 @@
 package net.darkhax.botanypots.addons.crt;
 
-import static net.darkhax.botanypots.BotanyPots.MOD_ID;
-
 import com.blamejared.crafttweaker.impl.commands.CTCommandCollectionEvent;
-import com.blamejared.crafttweaker.impl.commands.script_examples.ExampleCollectionEvent;
 
 import net.darkhax.botanypots.BotanyPotHelper;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class CraftTweakerEventSubscription {
-    
-    @SubscribeEvent
-    public static void addExampleScriptFiles (ExampleCollectionEvent event) {
-        
-        event.addResource(new ResourceLocation(MOD_ID, "botanypots/crops"));
-        event.addResource(new ResourceLocation(MOD_ID, "botanypots/fertilizers"));
-        event.addResource(new ResourceLocation(MOD_ID, "botanypots/soils"));
-    }
     
     @SubscribeEvent
     public static void addDumpCommands (CTCommandCollectionEvent event) {
