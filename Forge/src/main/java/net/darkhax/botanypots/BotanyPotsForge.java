a532aeca12d8c43fccda1c781a0f1e9fcbd8a6d6
Tyler Hancock <darklime@live.ca>
2022-08-04 23:26:29 -0600

    Add support for The One Probe.

--- Forge/src/main/java/net/darkhax/botanypots/BotanyPotsForge.java
+++ Forge/src/main/java/net/darkhax/botanypots/BotanyPotsForge.java
@@ -1,6 +1,11 @@
 package net.darkhax.botanypots;
 
+import net.darkhax.bookshelf.api.Services;
+import net.darkhax.botanypots.addons.top.TOPPlugin;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Constants.MOD_ID)
 public class BotanyPotsForge {
@@ -8,5 +13,14 @@ public class BotanyPotsForge {
     public BotanyPotsForge() {
 
         new BotanyPotsCommon();
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::loadModCompat);
+    }
+
+    private void loadModCompat(FMLCommonSetupEvent event) {
+
+        if (Services.PLATFORM.isModLoaded("theoneprobe")) {
+
+            InterModComms.sendTo("theoneprobe", "getTheOneProbe", TOPPlugin::new);
+        }
     }
 }
---
+++ Forge/src/main/java/net/darkhax/botanypots/addons/top/TOPPlugin.java
@@ -0,0 +1,73 @@
+package net.darkhax.botanypots.addons.top;
+
+import mcjty.theoneprobe.api.ElementAlignment;
+import mcjty.theoneprobe.api.IProbeHitData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.IProbeInfoProvider;
+import mcjty.theoneprobe.api.ITheOneProbe;
+import mcjty.theoneprobe.api.ProbeMode;
+import mcjty.theoneprobe.apiimpl.styles.ProgressStyle;
+import net.darkhax.botanypots.Constants;
+import net.darkhax.botanypots.block.BlockEntityBotanyPot;
+import net.darkhax.botanypots.block.inv.BotanyPotContainer;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.function.Function;
+
+public class TOPPlugin implements IProbeInfoProvider, Function<ITheOneProbe, Void> {
+
+    private static final ResourceLocation PLUGIN_ID = new ResourceLocation(Constants.MOD_ID, "top_pot_info");
+
+    @Override
+    public ResourceLocation getID() {
+
+        return PLUGIN_ID;
+    }
+
+    @Override
+    public Void apply(ITheOneProbe probeMod) {
+
+        probeMod.registerProvider(this);
+        return null;
+    }
+
+    @Override
+    public void addProbeInfo(ProbeMode mode, IProbeInfo info, Player player, Level level, BlockState state, IProbeHitData data) {
+
+        if (level.getBlockEntity(data.getPos()) instanceof BlockEntityBotanyPot pot) {
+
+            final BotanyPotContainer inv = pot.getInventory();
+
+            if (!inv.getSoilStack().isEmpty()) {
+
+                final IProbeInfo soilInfo = info.horizontal(info.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER));
+                soilInfo.item(inv.getSoilStack());
+                soilInfo.mcText(new TranslatableComponent("tooltip.botanypots.soil", inv.getSoilStack().getHoverName()));
+            }
+
+            if (!inv.getCropStack().isEmpty()) {
+
+                final IProbeInfo seedInfo = info.horizontal(info.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER));
+                seedInfo.item(inv.getCropStack());
+                seedInfo.mcText(new TranslatableComponent("tooltip.botanypots.seed", inv.getCropStack().getHoverName()));
+            }
+
+            if (pot.isGrowing()) {
+
+                final ProgressStyle style = new ProgressStyle();
+                style.filledColor(0xff32CD32);
+                style.alternateFilledColor(0xff259925);
+                style.prefix("tooltip.botanypots.progress");
+                style.suffix("%");
+
+                final int f = Mth.floor((float) (pot.getGrowthTime() / inv.getRequiredGrowthTime()) * 100f);
+                info.progress(f, 100, style);
+            }
+        }
+    }
+}
