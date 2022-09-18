da9a53dcb78029e6741ccb98c7d7bd2b0dc7cc3e
alpha <alphamodeyt@gmail.com>
2022-09-18 13:10:31 -0500

    Implement top for fabric

---
+++ Fabric/src/main/java/net/darkhax/botanypots/addons/top/TOPPlugin.java
@@ -0,0 +1,65 @@
+package net.darkhax.botanypots.addons.top;
+
+import mcjty.theoneprobe.api.*;
+import mcjty.theoneprobe.apiimpl.styles.ProgressStyle;
+import net.darkhax.botanypots.Constants;
+import net.darkhax.botanypots.block.BlockEntityBotanyPot;
+import net.darkhax.botanypots.block.inv.BotanyPotContainer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
+
+public class TOPPlugin implements IProbeInfoProvider, ITheOneProbePlugin {
+
+    private static final ResourceLocation PLUGIN_ID = new ResourceLocation(Constants.MOD_ID, "top_pot_info");
+
+    @Override
+    public ResourceLocation getID() {
+
+        return PLUGIN_ID;
+    }
+
+    @Override
+    public void onLoad(ITheOneProbe probeMod) {
+
+        probeMod.registerProvider(this);
+    }
+
+    @Override
+    public void addProbeInfo(ProbeMode mode, IProbeInfo info, Player player, Level level, BlockState state, IProbeHitData data) {
+
+        if (level.getBlockEntity(data.getPos()) instanceof BlockEntityBotanyPot pot) {
+
+            final BotanyPotContainer inv = pot.getInventory();
+
+            if (!inv.getSoilStack().isEmpty()) {
+
+                final IProbeInfo soilInfo = info.horizontal(info.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER));
+                soilInfo.item(inv.getSoilStack());
+                soilInfo.mcText(Component.translatable("tooltip.botanypots.soil", inv.getSoilStack().getHoverName()));
+            }
+
+            if (!inv.getCropStack().isEmpty()) {
+
+                final IProbeInfo seedInfo = info.horizontal(info.defaultLayoutStyle().alignment(ElementAlignment.ALIGN_CENTER));
+                seedInfo.item(inv.getCropStack());
+                seedInfo.mcText(Component.translatable("tooltip.botanypots.seed", inv.getCropStack().getHoverName()));
+            }
+
+            if (pot.isGrowing()) {
+
+                final ProgressStyle style = new ProgressStyle();
+                style.filledColor(0xff32CD32);
+                style.alternateFilledColor(0xff259925);
+                style.prefix("tooltip.botanypots.progress");
+                style.suffix("%");
+
+                final int f = Mth.floor((float) (pot.getGrowthTime() / inv.getRequiredGrowthTime()) * 100f);
+                info.progress(f, 100, style);
+            }
+        }
+    }
+}
