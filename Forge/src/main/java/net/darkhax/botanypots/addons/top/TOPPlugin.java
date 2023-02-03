0a3c4f1732cf916aad5fa11e33a2b0377416b78c
Tyler Hancock <darklime@live.ca>
2023-02-03 01:16:33 -0700

    Add timer to TOP HUD and hide progress/timer when fully grown. #292

--- Forge/src/main/java/net/darkhax/botanypots/addons/top/TOPPlugin.java
+++ Forge/src/main/java/net/darkhax/botanypots/addons/top/TOPPlugin.java
@@ -10,18 +10,24 @@ import mcjty.theoneprobe.apiimpl.styles.ProgressStyle;
 import net.darkhax.botanypots.Constants;
 import net.darkhax.botanypots.block.BlockEntityBotanyPot;
 import net.darkhax.botanypots.block.inv.BotanyPotContainer;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
+import net.minecraft.util.StringUtil;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
+import org.checkerframework.checker.units.qual.C;
 
 import java.util.function.Function;
 
 public class TOPPlugin implements IProbeInfoProvider, Function<ITheOneProbe, Void> {
 
     private static final ResourceLocation PLUGIN_ID = new ResourceLocation(Constants.MOD_ID, "top_pot_info");
+    private static final Component HARVESTABLE = new TranslatableComponent("tooltip.botanypots.harvestable").withStyle(ChatFormatting.GREEN);
 
     @Override
     public ResourceLocation getID() {
@@ -59,14 +65,28 @@ public class TOPPlugin implements IProbeInfoProvider, Function<ITheOneProbe, Voi
 
             if (pot.isGrowing()) {
 
-                final ProgressStyle style = new ProgressStyle();
-                style.filledColor(0xff32CD32);
-                style.alternateFilledColor(0xff259925);
-                style.prefix("tooltip.botanypots.progress");
-                style.suffix("%");
+                final int currentTime = pot.getGrowthTime();
+                final int requiredTime = inv.getRequiredGrowthTime();
+                final int remainingTime = requiredTime - currentTime;
 
-                final int f = Mth.floor((float) pot.getGrowthTime() / inv.getRequiredGrowthTime() * 100f);
-                info.progress(f, 100, style);
+                if (remainingTime > 0) {
+
+                    info.mcText(new TranslatableComponent("tooltip.botanypots.time", StringUtil.formatTickDuration(requiredTime - currentTime)));
+
+                    final ProgressStyle progressStyle = new ProgressStyle();
+                    progressStyle.filledColor(0xff32CD32);
+                    progressStyle.alternateFilledColor(0xff259925);
+                    progressStyle.prefix("tooltip.botanypots.progress");
+                    progressStyle.suffix("%");
+
+                    final int f = Mth.floor(((float) currentTime / requiredTime) * 100f);
+                    info.progress(f, 100, progressStyle);
+                }
+
+                else {
+
+                    info.mcText(HARVESTABLE);
+                }
             }
         }
     }
