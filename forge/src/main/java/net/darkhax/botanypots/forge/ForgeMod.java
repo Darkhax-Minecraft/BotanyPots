b12eb71fcbced47e9fa26d22eb61e806398e9c6b
Tyler Hancock <darklime@live.ca>
2025-05-13 23:36:44 -0600

    Remove support for Forge.

--- forge/src/main/java/net/darkhax/botanypots/forge/ForgeMod.java
+++
@@ -1,8 +0,0 @@
-package net.darkhax.botanypots.forge;
-
-import net.darkhax.botanypots.common.impl.BotanyPotsMod;
-import net.minecraftforge.fml.common.Mod;
-
-@Mod(BotanyPotsMod.MOD_ID)
-public class ForgeMod {
-}
